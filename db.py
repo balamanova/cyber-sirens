0b2a95b89b1a97e962d6a27a605371d3a99d8d5f
balamanova <asema.balamanova777@gmail.com>
2025-05-10 17:46:51 +0500

    db changes

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://cyber_sirens_obea_user:kkt3iNhpYWXA7QlcTWBuDD2vZYjFqDBZ@dpg-cvp8q5odl3ps73fue970-a.oregon-postgres.render.com/cyber_sirens_obea"
+SQLALCHEMY_DATABASE_URL = "postgresql://cyber_sirens_149d_user:pjp35K4HLrUjGurMQFkdPj0YU6Yc5smm@dpg-d0fkk1c9c44c73begr2g-a.oregon-postgres.render.com/cyber_sirens_149d"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
